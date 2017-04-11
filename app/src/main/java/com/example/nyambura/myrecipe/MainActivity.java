f5d2792fcf19a6edf38444772c0bdfa87f1f2974
annmunyare <annmunyare28@yahoo.com>
2017-04-11 11:19:05 +0300

    Main Activity

---
+++ app/src/main/java/com/example/nyambura/myrecipe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nyambura.myrecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
