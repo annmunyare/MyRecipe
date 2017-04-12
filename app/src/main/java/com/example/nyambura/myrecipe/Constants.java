ed4de32fb8bebf4af2575eb43fc7b105c5aba7ae
annmunyare <annmunyare28@yahoo.com>
2017-04-12 14:09:54 +0300

    edamam api configuration

---
+++ app/src/main/java/com/example/nyambura/myrecipe/Constants.java
@@ -0,0 +1,17 @@
+package com.example.nyambura.myrecipe;
+
+/**
+ * Created by nyambura on 4/12/17.
+ */
+
+public class Constants {
+    public static final String EDAMAM_API_KEY = BuildConfig.EDAMAM_API_KEY;
+    public static final String EDAMAM_API_ID = BuildConfig.EDAMAM_API_ID;
+    public static final String RECIPE_URL = "https://api.edamam.com/search";
+    public static final String SEARCHED_TERM = "q";
+    //will need to associate all of the with a uid
+
+    public static final String FIREBASE_CHILD_RECIPES = "recipes";
+    public static final String PREFERENCES_SEARCH_FOOD = "foodTitle";
+    public static final String FIREBASE_QUERY_INDEX = "index";
+}
